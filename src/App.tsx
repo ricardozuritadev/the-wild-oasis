d1ce71e634581c1e1d2a656f366d19b6f5e03ddf
Ricardo Zurita <ricardoz@clickandgo.com>
2024-06-27 16:16:09 +0200

    Modify App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,6 @@
-import GlobalStyles from "styles/global-styles";
-import Button from "ui/Button";
-
-import Heading from "ui/Heading";
-import Row from "ui/Row";
+import { RouterProvider } from "react-router-dom";
+import { router } from "routes/router";
 
 export default function App() {
-    return (
-        <>
-            <GlobalStyles />
-
-            <Row type="horizontal">
-                <Heading as="h1">The Wild Oasis</Heading>
-
-                <div>
-                    <Heading as="h2">Check in and out</Heading>
-                    <Heading as="h3">Form</Heading>
-                </div>
-            </Row>
-
-            <Button size="medium" variation="primary">
-                Click me
-            </Button>
-
-            <Button size="small" variation="secondary">
-                Click me
-            </Button>
-        </>
-    );
+    return <RouterProvider router={router} />;
 }
