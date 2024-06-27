16a25f735e83d86a77f004ecd9e219bf50d28f22
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-06-27 09:36:11 +0200

    Install styled components package

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+`;
+
+const Button = styled.button`
+    font-size: 1.4rem;
+    padding: 1.2rem 1.6rem;
+    font-weight: 500;
+    border: none;
+    border-radius: 7px;
+    background-color: purple;
+    color: white;
+`;
+
 export default function App() {
-    return <div>App</div>;
+    return (
+        <div>
+            <H1>The Wild Oasis</H1>
+            <Button>Click Me</Button>
+        </div>
+    );
 }
