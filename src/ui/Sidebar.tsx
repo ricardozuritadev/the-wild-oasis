927d69b2e01bc04859f77534c2337a95ff4cce76
Ricardo Zurita <ricardoz@clickandgo.com>
2024-06-27 17:32:46 +0200

    Implement Logo and MainNav components

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
+    display: flex;
+    flex-direction: column;
+    gap: 3.2rem;
     background-color: var(--color-grey-0);
     padding: 1.6rem 4.8rem;
     border-bottom: 1px solid var(--color-grey-100);
@@ -8,5 +13,10 @@ const StyledSidebar = styled.aside`
 `;
 
 export default function Sidebar() {
-    return <StyledSidebar>SIDEBAR</StyledSidebar>;
+    return (
+        <StyledSidebar>
+            <Logo />
+            <MainNav />
+        </StyledSidebar>
+    );
 }
