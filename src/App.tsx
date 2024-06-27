1fe6f2e3a6c6a506ac3fe6c953736b7d8a99b3bb
Ricardo Zurita <ricardoz@clickandgo.com>
2024-06-27 11:22:44 +0200

    Implement styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import styled from "styled-components";
+import GlobalStyles from "styles/global-styles";
+import Button from "ui/Button";
 
-const H1 = styled.h1`
-    font-size: 30px;
-    font-weight: 600;
-`;
-
-const Button = styled.button`
-    font-size: 1.4rem;
-    padding: 1.2rem 1.6rem;
-    font-weight: 500;
-    border: none;
-    border-radius: 7px;
-    background-color: purple;
-    color: white;
-`;
+import Heading from "ui/Heading";
+import Row from "ui/Row";
 
 export default function App() {
     return (
-        <div>
-            <H1>The Wild Oasis</H1>
-            <Button>Click Me</Button>
-        </div>
+        <>
+            <GlobalStyles />
+
+            <Row type="horizontal">
+                <Heading as="h1">The Wild Oasis</Heading>
+
+                <div>
+                    <Heading as="h2">Check in and out</Heading>
+                    <Heading as="h3">Form</Heading>
+                </div>
+            </Row>
+
+            <Button size="medium" variation="primary">
+                Click me
+            </Button>
+
+            <Button size="small" variation="secondary">
+                Click me
+            </Button>
+        </>
     );
 }
